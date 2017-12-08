a15c8c6beffa0dd7994d585bce4730877d3773d3
lethanhthuyak <lethanhthuy0803@gmail.com>
2017-12-08 10:02:00 +0700

    Add files via upload

---
+++ DonDatHang.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderFoodOnline.Models.Entity
+{
+    public class DonDatHang:BaseEntity
+    {
+        public string MaKhachHang { get; set; }
+        public DateTime NgayThang { get; set; }
+        public DateTime NgayGioGiao { get; set; }
+        public string DiaChiGiao { get; set; }
+        public string SoDienThoai { get; set; }
+        public string HinhThucThanhToan { get; set; }
+        public int TongTien { get; set; }
+        public string CTDonDatHang { get; set; }
+        public bool DaXacNhan{ get; set; }
+        public bool DaGiaoDich { get; set; }
+    }
+}
